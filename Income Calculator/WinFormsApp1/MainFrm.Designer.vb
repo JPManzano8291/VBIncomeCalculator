537fa9c2e6101733735ff94b0e9ea13282f52813
Joshy <87001480+JPManzano8291@users.noreply.github.com>
2024-03-05 14:56:18 +0800

    Add files via upload

---
+++ Income Calculator/WinFormsApp1/MainFrm.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainFrm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        gbStatus = New GroupBox()
+        rbGovOff = New RadioButton()
+        rbSelfEmp = New RadioButton()
+        rbEmp = New RadioButton()
+        gbInOpt = New GroupBox()
+        rbAnnInc = New RadioButton()
+        rbMonInc = New RadioButton()
+        tbInput = New TextBox()
+        Label1 = New Label()
+        tbBiMonPay = New TextBox()
+        tbNetMonPay = New TextBox()
+        tbNetAnnInc = New TextBox()
+        lblBiNetPay = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        btnCalc = New Button()
+        btnReset = New Button()
+        gbStatus.SuspendLayout()
+        gbInOpt.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' gbStatus
+        ' 
+        gbStatus.BackColor = SystemColors.ButtonFace
+        gbStatus.Controls.Add(rbGovOff)
+        gbStatus.Controls.Add(rbSelfEmp)
+        gbStatus.Controls.Add(rbEmp)
+        gbStatus.Location = New Point(39, 53)
+        gbStatus.Name = "gbStatus"
+        gbStatus.Size = New Size(266, 145)
+        gbStatus.TabIndex = 0
+        gbStatus.TabStop = False
+        gbStatus.Text = "Status"
+        ' 
+        ' rbGovOff
+        ' 
+        rbGovOff.AutoSize = True
+        rbGovOff.Location = New Point(20, 100)
+        rbGovOff.Name = "rbGovOff"
+        rbGovOff.Size = New Size(183, 25)
+        rbGovOff.TabIndex = 2
+        rbGovOff.TabStop = True
+        rbGovOff.Text = "Government Official"
+        rbGovOff.UseVisualStyleBackColor = True
+        ' 
+        ' rbSelfEmp
+        ' 
+        rbSelfEmp.AutoSize = True
+        rbSelfEmp.Location = New Point(20, 69)
+        rbSelfEmp.Name = "rbSelfEmp"
+        rbSelfEmp.Size = New Size(138, 25)
+        rbSelfEmp.TabIndex = 1
+        rbSelfEmp.TabStop = True
+        rbSelfEmp.Text = "Self Employed"
+        rbSelfEmp.UseVisualStyleBackColor = True
+        ' 
+        ' rbEmp
+        ' 
+        rbEmp.AutoSize = True
+        rbEmp.Location = New Point(20, 38)
+        rbEmp.Name = "rbEmp"
+        rbEmp.Size = New Size(105, 25)
+        rbEmp.TabIndex = 0
+        rbEmp.TabStop = True
+        rbEmp.Text = "Employed"
+        rbEmp.UseVisualStyleBackColor = True
+        ' 
+        ' gbInOpt
+        ' 
+        gbInOpt.BackColor = SystemColors.ButtonFace
+        gbInOpt.Controls.Add(rbAnnInc)
+        gbInOpt.Controls.Add(rbMonInc)
+        gbInOpt.Location = New Point(349, 53)
+        gbInOpt.Name = "gbInOpt"
+        gbInOpt.Size = New Size(266, 145)
+        gbInOpt.TabIndex = 1
+        gbInOpt.TabStop = False
+        gbInOpt.Text = "Input Option"
+        ' 
+        ' rbAnnInc
+        ' 
+        rbAnnInc.AutoSize = True
+        rbAnnInc.Location = New Point(31, 88)
+        rbAnnInc.Name = "rbAnnInc"
+        rbAnnInc.Size = New Size(144, 25)
+        rbAnnInc.TabIndex = 1
+        rbAnnInc.TabStop = True
+        rbAnnInc.Text = "Annual Income"
+        rbAnnInc.UseVisualStyleBackColor = True
+        ' 
+        ' rbMonInc
+        ' 
+        rbMonInc.AutoSize = True
+        rbMonInc.Location = New Point(31, 48)
+        rbMonInc.Name = "rbMonInc"
+        rbMonInc.Size = New Size(154, 25)
+        rbMonInc.TabIndex = 0
+        rbMonInc.TabStop = True
+        rbMonInc.Text = "Monthly Income"
+        rbMonInc.UseVisualStyleBackColor = True
+        ' 
+        ' tbInput
+        ' 
+        tbInput.Location = New Point(237, 269)
+        tbInput.Name = "tbInput"
+        tbInput.Size = New Size(182, 29)
+        tbInput.TabIndex = 2
+        tbInput.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(293, 245)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(72, 21)
+        Label1.TabIndex = 3
+        Label1.Text = "Amount"
+        ' 
+        ' tbBiMonPay
+        ' 
+        tbBiMonPay.Location = New Point(73, 401)
+        tbBiMonPay.Name = "tbBiMonPay"
+        tbBiMonPay.ReadOnly = True
+        tbBiMonPay.Size = New Size(156, 29)
+        tbBiMonPay.TabIndex = 4
+        tbBiMonPay.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' tbNetMonPay
+        ' 
+        tbNetMonPay.Location = New Point(258, 401)
+        tbNetMonPay.Name = "tbNetMonPay"
+        tbNetMonPay.ReadOnly = True
+        tbNetMonPay.Size = New Size(139, 29)
+        tbNetMonPay.TabIndex = 5
+        tbNetMonPay.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' tbNetAnnInc
+        ' 
+        tbNetAnnInc.Location = New Point(428, 401)
+        tbNetAnnInc.Name = "tbNetAnnInc"
+        tbNetAnnInc.ReadOnly = True
+        tbNetAnnInc.Size = New Size(156, 29)
+        tbNetAnnInc.TabIndex = 6
+        tbNetAnnInc.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' lblBiNetPay
+        ' 
+        lblBiNetPay.AutoSize = True
+        lblBiNetPay.Location = New Point(73, 433)
+        lblBiNetPay.Name = "lblBiNetPay"
+        lblBiNetPay.Size = New Size(156, 21)
+        lblBiNetPay.TabIndex = 7
+        lblBiNetPay.Text = "Bi-Monthly Netpay"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(258, 433)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(139, 21)
+        Label2.TabIndex = 8
+        Label2.Text = "Net Monthly Pay"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(428, 433)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(158, 21)
+        Label3.TabIndex = 9
+        Label3.Text = "Net Annual Income"
+        ' 
+        ' btnCalc
+        ' 
+        btnCalc.Location = New Point(196, 322)
+        btnCalc.Name = "btnCalc"
+        btnCalc.Size = New Size(109, 31)
+        btnCalc.TabIndex = 10
+        btnCalc.Text = "Calculate"
+        btnCalc.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(349, 322)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(109, 31)
+        btnReset.TabIndex = 11
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 21F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(663, 510)
+        Controls.Add(btnReset)
+        Controls.Add(btnCalc)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(lblBiNetPay)
+        Controls.Add(tbNetAnnInc)
+        Controls.Add(tbNetMonPay)
+        Controls.Add(tbBiMonPay)
+        Controls.Add(Label1)
+        Controls.Add(tbInput)
+        Controls.Add(gbInOpt)
+        Controls.Add(gbStatus)
+        Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Margin = New Padding(4)
+        Name = "Form1"
+        Text = "Net Income"
+        gbStatus.ResumeLayout(False)
+        gbStatus.PerformLayout()
+        gbInOpt.ResumeLayout(False)
+        gbInOpt.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents gbStatus As GroupBox
+    Friend WithEvents rbGovOff As RadioButton
+    Friend WithEvents rbSelfEmp As RadioButton
+    Friend WithEvents rbEmp As RadioButton
+    Friend WithEvents gbInOpt As GroupBox
+    Friend WithEvents rbAnnInc As RadioButton
+    Friend WithEvents rbMonInc As RadioButton
+    Friend WithEvents tbInput As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tbBiMonPay As TextBox
+    Friend WithEvents tbNetMonPay As TextBox
+    Friend WithEvents tbNetAnnInc As TextBox
+    Friend WithEvents lblBiNetPay As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnReset As Button
+
+End Class
---
+++ Income Calculator/WinFormsApp1/MainFrm.vb
@@ -0,0 +1,309 @@
+﻿Public Class MainFrm
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        ClearInputs()
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        'Validate inputs
+        If Not ValidateInputs() Then Exit Sub
+        Dim salary As Decimal = Decimal.Parse(tbInput.Text)
+        Try
+            If rbMonInc.Checked Then
+
+                'Variables
+                Dim yrIncome As Decimal = salary * 12
+                Dim excess As Decimal
+                Dim taxexcess As Decimal
+                Dim ttlAnnual As Decimal
+                Dim ttlPagIbigCon As Decimal
+                Dim ttlGSISCon As Decimal
+                Dim ttlSSSCon As Decimal
+                Dim ttlPHealth As Decimal
+                Dim ttlConTax As Decimal
+                Dim netAnnInc As Decimal
+                Dim netMonthlyPay As Decimal
+                Dim netBiMonPay As Decimal
+
+                ' Annual Tax
+                If yrIncome > 250000.0 And yrIncome <= 400000.0 Then
+
+                    excess = yrIncome - 250000.0
+                    If excess > 0 Then
+                        taxexcess = 0.15 * excess
+                    End If
+
+                    ttlAnnual = taxexcess
+
+                ElseIf yrIncome > 400000 And yrIncome <= 800000 Then
+
+                    excess = yrIncome - 400000
+                    If excess > 0 Then
+                        taxexcess = 0.2 * excess
+                    End If
+                    ttlAnnual = 22500.0 + taxexcess
+
+                ElseIf yrIncome > 800000 And yrIncome <= 2000000 Then
+
+                    excess = yrIncome - 800000
+
+                    If excess > 0 Then
+                        taxexcess = 0.25 * excess
+                    End If
+
+                    ttlAnnual = 102500.0 + taxexcess
+
+                ElseIf yrIncome > 2000000 And yrIncome <= 8000000 Then
+
+                    excess = yrIncome - 2000000
+                    If excess > 0 Then
+                        taxexcess = 0.3 * excess
+                    End If
+                    ttlAnnual = 402500.0 + taxexcess
+
+                ElseIf yrIncome > 8000000 Then
+
+                    excess = yrIncome - 8000000
+                    If excess > 0 Then
+                        taxexcess = 0.35 * excess
+                    End If
+                    ttlAnnual = 2202500.0 + taxexcess
+
+                End If
+
+                ' Pag-Ibig
+                If salary <= 1500 Then
+
+                    Dim tax As Decimal = salary * 0.01
+                    ttlPagIbigCon = tax * 12
+
+                ElseIf salary > 1500 Then
+
+                    Dim tax As Decimal = salary * 0.02
+                    ttlPagIbigCon = tax * 12
+
+                End If
+
+                'GSIS if Government Official
+
+                If rbGovOff.Checked Then
+
+                    Dim month = salary * 0.09
+                    ttlGSISCon = month * 12
+
+                Else
+                    'SSS when monthly salary is min 4000 and max 30000
+                    If salary >= 4000 And salary <= 30000 Then
+
+                        Dim month = salary * 0.14
+                        ttlSSSCon = month * 12
+
+                    End If
+
+                End If
+
+                If salary >= 10000 And salary <= 100000 Then
+
+                    Dim month = salary * 0.05
+                    ttlPHealth = month * 12
+
+                End If
+
+                'Total Contribution or Tax
+                ttlConTax = ttlAnnual + ttlPagIbigCon + ttlSSSCon + ttlGSISCon + ttlPHealth
+
+                netAnnInc = yrIncome - ttlConTax
+                netMonthlyPay = netAnnInc / 12
+                netBiMonPay = netMonthlyPay / 2
+
+
+                tbBiMonPay.Text = netBiMonPay.ToString("0.00")
+                tbNetMonPay.Text = netMonthlyPay.ToString("0.00")
+                tbNetAnnInc.Text = netAnnInc.ToString("0.00")
+
+                'Calculation for Annual Income Input
+            Else
+
+                'This are Variables for calculation
+                Dim yearIncome As Double = salary
+                Dim excess As Double
+                Dim taxExcess As Double
+                Dim totalAnnualTax As Double
+                Dim totalPagIbigCon As Double
+                Dim totalGSISCon As Double
+                Dim totalSSSCon As Double
+                Dim totalPhilHealth As Double
+                Dim totalContri_Tax As Double
+                'Outputs
+                Dim netAnnualIncome As Double
+                Dim netMonPay As Double
+                Dim netBiMonPay As Double
+
+                'Calculation For Annual Tax
+                If yearIncome > 250000.0 And yearIncome <= 400000.0 Then
+
+                    Dim taxRate As Double = 0.15
+                    excess = yearIncome - 250000.0
+
+
+                    If excess > 0 Then
+                        taxExcess = taxRate * excess
+                    End If
+
+                    totalAnnualTax = taxExcess
+
+                ElseIf yearIncome > 400000 And yearIncome <= 800000 Then
+
+                    Dim taxRate As Double = 0.2
+                    Dim fixTax As Double = 22500.0
+
+                    excess = yearIncome - 400000
+
+                    If excess > 0 Then
+                        taxExcess = taxRate * excess
+                    End If
+
+                    totalAnnualTax = fixTax + taxExcess
+
+                ElseIf yearIncome > 800000 And yearIncome <= 2000000 Then
+
+                    Dim taxRate As Double = 0.25
+                    Dim fixTax As Double = 102500.0
+
+                    excess = yearIncome - 800000
+
+                    If excess > 0 Then
+                        taxExcess = taxRate * excess
+                    End If
+
+                    totalAnnualTax = fixTax + taxExcess
+
+                ElseIf yearIncome > 2000000 And yearIncome <= 8000000 Then
+
+                    Dim taxRate As Double = 0.3
+                    Dim fixTax As Double = 402500.0
+
+                    excess = yearIncome - 2000000
+
+                    If excess > 0 Then
+                        taxExcess = taxRate * excess
+                    End If
+
+                    totalAnnualTax = fixTax + taxExcess
+
+                ElseIf yearIncome > 8000000 Then
+
+                    Dim taxRate As Double = 0.35
+                    Dim fixTax As Double = 2202500.0
+
+                    excess = yearIncome - 8000000
+
+                    If excess > 0 Then
+                        taxExcess = taxRate * excess
+                    End If
+
+                    totalAnnualTax = fixTax + taxExcess
+                End If
+
+                'Conversion from annual to Monthly
+                salary = salary / 12
+
+                'Calculation for Pag Ibig
+                If salary <= 1500 Then
+
+                    Dim taxRate As Double = 0.01
+                    Dim tax As Double
+
+                    tax = salary * taxRate
+                    totalPagIbigCon = tax * 12
+
+                ElseIf salary > 1500 Then
+
+                    Dim taxRate As Double = 0.02
+                    Dim tax As Double
+
+                    tax = salary * taxRate
+                    totalPagIbigCon = tax * 12
+
+                End If
+
+                ' GSIS if Government Official
+
+                If rbGovOff.Checked Then
+
+                    Dim month = salary * 0.09
+
+                    totalGSISCon = month * 12
+
+                Else
+                    'Calculation for SSS when monthly salary is min 4000 and max 30000
+                    If salary >= 4000 And salary <= 30000 Then
+
+                        Dim month = salary * 0.14
+                        totalSSSCon = month * 12
+
+                    End If
+
+                End If
+
+                If salary >= 10000 And salary <= 100000 Then
+
+                    Dim month = salary * 0.05
+                    totalPhilHealth = month * 12
+
+                End If
+
+                'Calculation for total Contribution or Tax
+                totalContri_Tax = totalAnnualTax + totalPagIbigCon + totalSSSCon + totalGSISCon + totalPhilHealth
+
+                netAnnualIncome = yearIncome - totalContri_Tax
+                netMonPay = netAnnualIncome / 12
+                netBiMonPay = netMonPay / 2
+
+                tbBiMonPay.Text = netBiMonPay.ToString("0.00")
+                tbNetMonPay.Text = netMonPay.ToString("0.00")
+                tbNetAnnInc.Text = netAnnualIncome.ToString("0.00")
+
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Please Enter Valid Input Or Number for Amount")
+        End Try
+
+    End Sub
+
+    Private Function ValidateInputs() As Boolean
+        ' Validate employment status
+        If Not (rbEmp.Checked OrElse rbSelfEmp.Checked OrElse rbGovOff.Checked) Then
+            MessageBox.Show("Please select an employment status.")
+            Return False
+        End If
+
+        ' Validate income option
+        If Not (rbMonInc.Checked OrElse rbAnnInc.Checked) Then
+            MessageBox.Show("Please select an income option.")
+            Return False
+        End If
+
+        ' Validate income input
+        If String.IsNullOrWhiteSpace(tbInput.Text) Then
+            MessageBox.Show("Please enter the income.")
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Private Sub ClearInputs()
+        ' Clear all inputs
+        rbEmp.Checked = False
+        rbSelfEmp.Checked = False
+        rbGovOff.Checked = False
+        rbMonInc.Checked = False
+        rbAnnInc.Checked = False
+        tbInput.Clear()
+        tbBiMonPay.Clear()
+        tbNetMonPay.Clear()
+        tbNetAnnInc.Clear()
+    End Sub
+End Class
